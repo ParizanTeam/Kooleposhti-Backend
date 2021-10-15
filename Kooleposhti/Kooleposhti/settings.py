e075a8d924ee799295591525a5ecc445b518b003
Your Name <you@example.com>
2021-10-15 12:26:55 +0330

    fixing database

--- Kooleposhti/Kooleposhti/settings.py
+++ Kooleposhti/Kooleposhti/settings.py
@@ -79,18 +79,8 @@ WSGI_APPLICATION = 'Kooleposhti.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''DATABASES = {
-    'default': env.dj_db_url("DATABASE_URL")
-}'''
 DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
              }
-
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
 
 
 # Password validation
