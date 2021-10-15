d9822cc7f1c3711c1fa0f3dcf1f1c29b9c42d6e1
Your Name <you@example.com>
2021-10-15 12:31:02 +0330

    fixing database

--- Kooleposhti/Kooleposhti/settings.py
+++ Kooleposhti/Kooleposhti/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'Kooleposhti.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-             }
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation
