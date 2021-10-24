a8d6d3f62317291308faa1c72ad54f0e6fb2e935
MohammadMahdi Javid <you@example.com>
2021-10-24 23:22:50 +0330

    fixing settings

--- Kooleposhti/Kooleposhti/settings.py
+++ Kooleposhti/Kooleposhti/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
