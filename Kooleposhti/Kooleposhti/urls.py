715c551ac10571c46fe69d145ea6f306daf1c5da
MohammadMahdi Javid <you@example.com>
2021-10-26 11:31:17 +0330

    fixing heroku

--- Kooleposhti/Kooleposhti/urls.py
+++ Kooleposhti/Kooleposhti/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from rest_framework.schemas import get_schema_view
+# from rest_framework.schemas import get_schema_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
