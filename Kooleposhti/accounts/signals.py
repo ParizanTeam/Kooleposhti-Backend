3935bf48b6dde9a19b4a5eeb3d3709cd297a2ab4
MohammadMahdi Javid <you@example.com>
2021-10-27 21:05:43 +0330

    creating custom signal

--- Kooleposhti/accounts/signals.py
+++
@@ -1,9 +0,0 @@
-from django.dispatch import receiver
-from .models import Student, Instructor
-from django.db.models.signals import post_save
-
-
-@receiver(post_save)
-def create_student_for_new_user(sender, *args, **kwargs):
-    if kwargs['created']:
-        Student.objects.create(user=kwargs.get['instance'])
