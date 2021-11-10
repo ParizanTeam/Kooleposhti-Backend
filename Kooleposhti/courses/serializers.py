a008c69c02076516499415d36631965237728ffc
Ghazal Bakhshande <Ghazal.best76@gmail.com>
2021-11-10 08:34:28 +0330

    fix-conflict

--- Kooleposhti/courses/serializers.py
+++ Kooleposhti/courses/serializers.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 from accounts.models import Instructor
 from djoser.serializers import UserSerializer as BaseUserSerializer
 from accounts.serializers import StudentSerializer, InstructorSerializer
+import jdatetime
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -12,22 +13,67 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = '__all__'
 
+    # def create(self, validated_data):
+    #     course_id = self.context.get('course_id')
+    #     return Tag.objects.create( course_id=course_id, **validated_data)
+
+
+class GoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    # def create(self, validated_data):
+    #     course_id = self.context.get('course_id')
+    #     return Goal.objects.create( course_id=course_id, **validated_data)
+
+
 class ChapterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chapter
         fields = '__all__'
 
+    # def create(self, validated_data):
+    #     course_id = self.context.get('course_id')
+    #     return Chapter.objects.create( course_id=course_id, **validated_data)
+
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = '__all__'
+
+    def create(self, validated_data):
+        # course_id = self.context.get('course_id')
+        date = validated_data.pop('date')
+        date = jdatetime.date(*date.split('/')).togregorian()
+        return Class.objects.create(date=date, **validated_data)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        student = request.user
+        return Comment.objects.create(student=student, **validated_data)
+
+
 class CourseSerializer(serializers.ModelSerializer):
     chapter = ChapterSerializer(many=True)
     students = StudentSerializer(many=True)
     tags = TagSerializer(many=True)
+    classes = ClassSerializer(many=True)
+    instructor = InstructorSerializer()
     class Meta:
         model = Course
         fields = '__all__'
         # fields = ('title', 'description', 'price',
         #           'last_update', 'instructor', 'new_price')  # __all__
-    instructor = serializers.HyperlinkedRelatedField(
-        queryset=Instructor.objects.all(), view_name='instructor-detail')
+    # instructor = serializers.HyperlinkedRelatedField(
+    #     queryset=Instructor.objects.all(), view_name='instructor-detail')
     new_price = serializers.SerializerMethodField(
         method_name='calculate_new_price')
 
@@ -35,12 +81,25 @@ class CourseSerializer(serializers.ModelSerializer):
         return course.price * Decimal(1.1)
 
     def create(self, validated_data):
-        course = super().create_(validated_data)
         request = self.context.get("request")
         instructor = request.user
-        course.instructor = instructor
-        course.save()
+        category = Category.objects.filter(title=validated_data.pop('category'))
+        course = Course.objects.create( instructor=instructor, category=category, **validated_data)
+        tags = validated_data.pop('tags')
+        for tag in tags:
+            Tag.objects.create(course=course, **tag)
+        goals = validated_data.pop('goals')
+        for goal in goals:
+            Goal.objects.create(course=course, **goal)
+        # chapters = validated_data.pop('chapters')
+        # for chapter in chapters:
+        #     Chapter.objects.create(course=course, **chapter)
+        classes = validated_data.pop('classes')
+        for myclass in classes:
+            Class.objects.create(course=course, **myclass)
+        
         return course
+
 
 
 
