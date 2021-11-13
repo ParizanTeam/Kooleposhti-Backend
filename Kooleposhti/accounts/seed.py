9815a42fdd6a704b460efd5b6eabae301278927e
MohammadMahdi Javid <you@example.com>
2021-11-13 21:36:59 +0330

    fixing migrations

--- Kooleposhti/accounts/seed.py
+++ Kooleposhti/accounts/seed.py
@@ -16,5 +16,13 @@ s = User.objects.create_user(
     username='mahdi4', password='123asdnjkasdu123890', email='mahdi4@gmail.com')
 s = Student.objects.create(user=s)
 ct = Category(title='Education', slug=slugify('accounts-instructor'))
-c = Course.objects.create(title='python', description='python course', instructor=Instructor.objects.first(), price=100, enrollment_start_date=datetime.strptime('2020-01-01', "%Y-%m-%d"), enrollment_end_date=datetime.strptime('2020-12-31', "%Y-%m-%d"),
-                          start_date=datetime.strptime('2020-01-01', "%Y-%m-%d"), end_date=datetime.strptime('2020-12-31', "%Y-%m-%d"), start_class=datetime.strptime('2020-01-01', "%Y-%m-%d"), end_class=datetime.strptime('2020-12-31', "%Y-%m-%d"), max_students=10, min_age=18, max_age=60, category=ct)
+c = Course.objects.create(title='python', description='python course',
+                          instructor=Instructor.objects.first(), price=100,
+                          start_date=datetime.strptime(
+                              '2020-01-01', "%Y-%m-%d"),
+                          end_date=datetime.strptime('2020-12-31', "%Y-%m-%d"),
+                          max_students=10, min_age=18, max_age=60,
+                          category=Category.objects.first(),
+                          slug='slug course 1',
+                          duration=10,
+                          capacity=10)
