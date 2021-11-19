34f4e0839d344dbf401af1c04f742f93d2481144
MohammadMahdi Javid <you@example.com>
2021-11-19 23:38:31 +0330

    fixing dependency problem

--- track_changes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git init
-git remote add heroku_logger https://${GIT_TOKEN}@github.com/ParizanTeam/Track-Changes.git
-git config user.name "heroku"
-git config user.email "heroku@example.com"
-git pull heroku_logger master
