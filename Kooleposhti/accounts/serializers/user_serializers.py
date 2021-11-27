62aeeaf007292ccb9bf3e1dc3a9b402e508b72b6
MohammadMahdi Javid <you@example.com>
2021-11-27 23:41:28 +0330

    changing profile password options

--- Kooleposhti/accounts/serializers/user_serializers.py
+++ Kooleposhti/accounts/serializers/user_serializers.py
@@ -104,7 +104,6 @@ class BaseUserSerializer(serializers.ModelSerializer):
         instance.user.image = image
         instance.save()
         instance.user.save()
-        instance.user.image.save()
 
         # MyImage.objects.get(image=)
         # if isinstance(image['image'], InMemoryUploadedFile) :
