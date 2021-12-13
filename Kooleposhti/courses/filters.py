1805d3e69ae88562e9d4c92180610daa801c482c
Ghazal Bakhshande <Ghazal.best76@gmail.com>
2021-12-13 23:02:23 +0330

    serach and filters

--- Kooleposhti/courses/filters.py
+++ Kooleposhti/courses/filters.py
@@ -1,12 +1,22 @@
 
 from django_filters.rest_framework import FilterSet
-from .models import Course
+from .models import Course, Category
+import django_filters
+
+
 
 
 class CourseFilter(FilterSet):
+    # min_price = django_filters.NumberFilter(name="price", lookup_type='gte')
+    # max_price = django_filters.NumberFilter(name="price", lookup_type='lte')
+    # mm = django_filters.LookupChoiceFilter(field_name="categories")
     class Meta:
         model = Course
+        # fields = ['mm']
         fields = {
-            'instructor_id': ['exact'],
-            'price': ['gt', 'lt']
+            # 'instructor_id': ['exact'],
+            'categories': ['contains'],
+            'price': ['gt', 'lt'],
+            'start_date': ['gt', 'lt'],   
+            'max_age': ['gt', 'lt']
         }
