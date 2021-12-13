e61d5f573fc4ea3ac2cd76e4eb2e9e4d91ace30b
Ghazal Bakhshande <Ghazal.best76@gmail.com>
2021-12-14 00:33:48 +0330

    filter age

--- Kooleposhti/courses/filters.py
+++ Kooleposhti/courses/filters.py
@@ -7,16 +7,18 @@ import django_filters
 
 
 class CourseFilter(FilterSet):
-    # min_price = django_filters.NumberFilter(name="price", lookup_type='gte')
-    # max_price = django_filters.NumberFilter(name="price", lookup_type='lte')
+    age_lte = django_filters.NumberFilter(field_name="max_age", lookup_expr='lte')
+    age_gte = django_filters.NumberFilter(field_name="min_age", lookup_expr='gte')
     # mm = django_filters.LookupChoiceFilter(field_name="categories")
     class Meta:
         model = Course
         # fields = ['mm']
+        fields = ['age_lte', 'age_gte']
         fields = {
             # 'instructor_id': ['exact'],
             'categories': ['contains'],
             'price': ['gt', 'lt'],
             'start_date': ['gt', 'lt'],   
-            'max_age': ['gt', 'lt']
+            # 'max_age': ['gte'],
+            # 'min_age': ['lte'],
         }
