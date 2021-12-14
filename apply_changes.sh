065920b697f4b36776950193afd5754c708534c1
MohammadMahdiJavid <MohammadMahdiJavid>
2021-12-14 12:56:12 +0000

    new deploy at

--- apply_changes.sh
+++ apply_changes.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+git checkout deploy
+
+git add .
+
+NOW=$( date + '+%F_%H:%M:%S' )
+
+git commit -m "new deploy at $NOW"
+
 git pull
+
+cd Kooleposhti
+
+source /home/ubuntu/.local/share/virtualenvs/Kooleposhti-CSvIaxZK/bin/activate
+
+pipenv sync
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+cd ..
+
+pip install -r requirements.txt
 
 systemctl restart nginx
 
